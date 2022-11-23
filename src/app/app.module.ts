ccb8e73b1b5bea1a3701a859b35205355feb74de
ARULSARAVANAN1 <115278739+ARULSARAVANAN1@users.noreply.github.com>
2022-11-23 21:51:22 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+
+const routes:Routes = [  
+    {path:'home',component:HomeComponent},
+    {path:'dashboard',component:DashboardComponent},
+    {path:'login',component:LoginComponent}
+  
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
